306842db66f8913371d9b2e04ac5e4388e26044a
sja0929 <55110127+sja0929@users.noreply.github.com>
2019-10-01 23:07:02 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import Demo from "./Demo";
+// import Demo from "./Demo";
 import articles from "./data/articles.json";
 
-const querystring = window.location.search || "";
 
-if (querystring.toLowerCase() === "?demo=true") {
-  ReactDOM.render(<Demo />, document.getElementById("root"));
-} else {
-  ReactDOM.render(
-    <App article={Object.values(articles)[0]} />,
+ReactDOM.render(
+    <App articles={Object.values(articles)} />,
     document.getElementById("root")
-  );
-}
+);
+
